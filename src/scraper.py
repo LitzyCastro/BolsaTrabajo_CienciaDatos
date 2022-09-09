ddfacaa57985be64094dc445d1ad01ef495a0bee
George <jorge.araya.vergara@gmail.com>
2022-09-09 09:53:54 -0300

    Update scraper.py
    ok

--- src/scraper.py
+++ src/scraper.py
@@ -18,7 +18,7 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium import webdriver
 from bs4 import BeautifulSoup as bs
-
+import os
 
 # Gráficos
 # ==============================================================================
